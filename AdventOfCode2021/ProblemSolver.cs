3624abcb3246c9e4b7ad2688fcf3b7f266bf1389
Justin Ellery <Cellery72@users.noreply.github.com>
2021-12-28 14:42:47 -0500

    Day 7 Part 1 done.

--- AdventOfCode2021/ProblemSolver.cs
+++ AdventOfCode2021/ProblemSolver.cs
@@ -763,7 +763,7 @@ namespace AdventOfCode2021
             }
 
             int count = 0;
-             foreach (var line in board)
+            foreach (var line in board)
                 for (int i = 0; i < line.Count; ++i)
                     if (line[i] != "." && (int.Parse(line[i]) >= 2))
                         count++;
@@ -824,7 +824,7 @@ namespace AdventOfCode2021
          */
         public static int Day6(List<string> input)
         {
-            List<int> initialPool= input[0].Split(',').Select(i => int.Parse(i)).ToList();
+            List<int> initialPool = input[0].Split(',').Select(i => int.Parse(i)).ToList();
             return Lanternfish.CalculateLanternfishReproduction(initialPool, 80);
         }
         /*
@@ -854,13 +854,59 @@ namespace AdventOfCode2021
             foreach (int i in initialPool)
                 contents[i]++;
 
-            return Lanternfish.CalculateLanternFishReproudctionRecursively(contents,256);
-            //return Lanternfish.CalculateLanternFishReproudctionRecursively(startingPool, 256);
+            return Lanternfish.CalculateLanternFishReproudctionRecursively(contents, 256);
         }
 
-        public static int Day7(List<string> input)
+        /*
+         *      --- Day 7: The Treachery of Whales ---
+                A giant whale has decided your submarine is its next meal, and it's much faster than you are. There's nowhere to run!
+
+                Suddenly, a swarm of crabs (each in its own tiny submarine - it's too deep for them otherwise) zooms in to rescue you! They seem to be preparing to blast a hole in the ocean floor; sensors indicate a massive underground cave system just beyond where they're aiming!
+
+                The crab submarines all need to be aligned before they'll have enough power to blast a large enough hole for your submarine to get through. However, it doesn't look like they'll be aligned before the whale catches you! Maybe you can help?
+
+                There's one major catch - crab submarines can only move horizontally.
+
+                You quickly make a list of the horizontal position of each crab (your puzzle input). Crab submarines have limited fuel, so you need to find a way to make all of their horizontal positions match while requiring them to spend as little fuel as possible.
+
+                For example, consider the following horizontal positions:
+
+                16,1,2,0,4,2,7,1,2,14
+                This means there's a crab with horizontal position 16, a crab with horizontal position 1, and so on.
+
+                Each change of 1 step in horizontal position of a single crab costs 1 fuel. You could choose any horizontal position to align them all on, but the one that costs the least fuel is horizontal position 2:
+
+                Move from 16 to 2: 14 fuel
+                Move from 1 to 2: 1 fuel
+                Move from 2 to 2: 0 fuel
+                Move from 0 to 2: 2 fuel
+                Move from 4 to 2: 2 fuel
+                Move from 2 to 2: 0 fuel
+                Move from 7 to 2: 5 fuel
+                Move from 1 to 2: 1 fuel
+                Move from 2 to 2: 0 fuel
+                Move from 14 to 2: 12 fuel
+                This costs a total of 37 fuel. This is the cheapest possible outcome; more expensive outcomes include aligning at position 1 (41 fuel), position 3 (39 fuel), or position 10 (71 fuel).
+
+                Determine the horizontal position that the crabs can align to using the least fuel possible. How much fuel must they spend to align to that position?
+         */
+        public static long Day7(List<string> input)
         {
-            return -1;
+            List<int> horizontalPositions = input[0].Split(',').Select(i => int.Parse(i)).ToList();
+            Dictionary<long, long> results = new Dictionary<long, long>();
+
+            for (int i = horizontalPositions.Min(); i < horizontalPositions.Max(); ++i)
+            {
+                long totalFuelRequired = 0;
+                Dictionary<long, long> dict = horizontalPositions.Where(h => h != i).ToList().GroupBy(k => k).ToDictionary(g => (long)g.Key, g => (long)g.Count());
+
+                foreach (var kvp in dict)
+                    totalFuelRequired += (kvp.Key > i) ? (kvp.Key - i) * kvp.Value : (i - kvp.Key) * kvp.Value;
+
+                results.Add(i, totalFuelRequired);
+            }
+
+            return results.OrderBy(kvp => kvp.Value).First().Value;
         }
     }
 }
